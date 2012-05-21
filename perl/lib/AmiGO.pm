de5b0b35f66d971c5a2123214dbdd8d39e9dd98d
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2012-05-21 19:18:46 +0000

    path fix
    git-svn-id: https://geneontology.svn.sourceforge.net/svnroot/geneontology/AmiGO/trunk@4505 6f0e8829-b336-0410-acfb-cb9b228023ad

--- perl/lib/AmiGO.pm
+++ perl/lib/AmiGO.pm
@@ -1403,7 +1403,7 @@ sub golr_configuration {
 
   my $self = shift;
   my $json_file = $self->amigo_env('AMIGO_HTDOCS_ROOT_DIR') .
-    '/javascript/bbop/amigo/golr_meta.json';
+    '/javascript/bbop/golr_meta.json';
 
   #$self->kvetch("looking up conf: " . $json_file);
 
