996d83401e1b19119d01b7aa6519e52209dc263a
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2012-09-21 01:18:01 +0000

    docs and small cleaning
    git-svn-id: https://geneontology.svn.sourceforge.net/svnroot/geneontology/AmiGO/trunk@4597 6f0e8829-b336-0410-acfb-cb9b228023ad

--- javascript/TermDetailsBeta.js
+++ javascript/TermDetailsBeta.js
@@ -23,9 +23,9 @@ function TermDetailsInit(){
     // var prev_source_count = 0;
     // var prev_species_count = 0;
 
-    // Bring in meta information.
-    var species_data = gm.species();
-    var source_data = gm.sources();
+    // // Bring in meta information.
+    // var species_data = gm.species();
+    // var source_data = gm.sources();
 
     ///
     /// Tabify the layout if we can (may be in a non-tabby version).
@@ -53,10 +53,10 @@ function TermDetailsInit(){
 
     var solr_server = gm.golr_base();
 
-    // TODO: new to replace the below.
+    // Setup the annotation profile and make the annotation document
+    // category and the current acc sticky in the filters.
     var gm_ann = new bbop.golr.manager(solr_server, gconf);
     gm_ann.set_personality('bbop_ann'); // profile in gconf
-    // Make these two base filters sticky in this case.
     gm_ann.add_query_filter('document_category', 'annotation', ['*']);
     gm_ann.add_query_filter('isa_partof_closure', global_acc, ['*']);
 
@@ -69,22 +69,17 @@ function TermDetailsInit(){
     /// Setup and bind them together.
     ///
 
-    // Class/term init.
-    // gm_ann.register('reset', 'control_init_q',
-    // 		    ui_ann.make_filter_controls_frame, 0);
     // Setup the gross frames for the filters and results.
     ui_ann.setup_current_filters();
     ui_ann.setup_accordion();
     ui_ann.setup_results({'meta': true});
 
-    // Things to do on every reset event.
-    //gm_ann.register('reset', 'results_init', ui_ann.make_results_frame, -2);
-    //gm_ann.register('reset', 'filters_set', ui_ann.set_static_filters);
+    // Things to do on every reset event. Essentially re-draw
+    // everything.
     gm_ann.register('reset', 'curr_first', ui_ann.draw_current_filters, -1);
     gm_ann.register('reset', 'accordion_first', ui_ann.draw_accordion, -1);
     gm_ann.register('reset', 'meta_first', ui_ann.draw_meta, -1);
     gm_ann.register('reset', 'results_first', ui_ann.draw_results, -1);
-    // gm_ann.register('reset', 'results_init_after', _peg_q, -4);
 
     // Things to do on every search event.
     gm_ann.register('search', 'curr_filters_std', ui_ann.draw_current_filters);
@@ -94,10 +89,6 @@ function TermDetailsInit(){
 
     // Things to do on an error.
     gm_ann.register('error', 'results_unusual', ui_ann.draw_error);
-
-    // // Whenever there is a user interface action, trigger a model
-    // // search.
-    // ui_ann.register('action', 'ui_action', gm_ann.search);
 
     // Start the ball with a reset event.
     gm_ann.reset();
