c757a46ace8576e3ed122a76b409649129f69228
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2012-10-30 22:51:11 +0000

    bracket refactoring; temporarily patched loop in make after fail
    git-svn-id: https://geneontology.svn.sourceforge.net/svnroot/geneontology/AmiGO/trunk@4736 6f0e8829-b336-0410-acfb-cb9b228023ad

--- javascript/lib/amigo/data/statistics.js
+++ javascript/lib/amigo/data/statistics.js
@@ -23,18 +23,18 @@ bbop.core.namespace('amigo', 'data', 'statistics');
  * 
  * TBD
  */
-amigo.data.statistics.annotation_source = [["MGI", 140675], ["UniProtKB", 94446], ["ZFIN", 86096], ["PomBase", 31869], ["RGD", 23092], ["dictyBase", 20566], ["GOC", 6234], ["RefGenome", 5312], ["BHF-UCL", 4432], ["IntAct", 772], ["HGNC", 473], ["DFLAT", 304], ["PINC", 17], ["Roslin_Institute", 10], ["ENSEMBL", 2], ["Reactome", 1]];
+amigo.data.statistics.annotation_source = [["MGI", 141633], ["UniProtKB", 95782], ["ZFIN", 86095], ["PomBase", 31869], ["RGD", 23206], ["dictyBase", 20566], ["GOC", 6231], ["BHF-UCL", 4526], ["RefGenome", 2740], ["IntAct", 790], ["HGNC", 486], ["DFLAT", 304], ["PINC", 17], ["Roslin_Institute", 10], ["ENSEMBL", 2], ["Reactome", 1]];
 
 /*
  * Variable: annotation_source
  * 
  * TBD
  */
-amigo.data.statistics.annotation_evidence = [["similarity evidence", 105129], ["experimental evidence", 99636], ["curator inference", 54629]];
+amigo.data.statistics.annotation_evidence = [["similarity evidence", 103360], ["experimental evidence", 100456], ["curator inference", 54530]];
 
 /*
  * Variable: annotation_overview
  * 
  * TBD
  */
-amigo.data.statistics.annotation_overview = [["Source", "similarity evidence", "experimental evidence", "curator inference", "author statement", "combinatorial evidence", "genomic context evidence", "biological system reconstruction", "imported information"], ["MGI", 51772, 53474, 33293, 0, 0, 0, 0, 0], ["ZFIN", 0, 10202, 11115, 0, 0, 0, 0, 0], ["PomBase", 10121, 14791, 3570, 2665, 0, 0, 0, 0], ["dictyBase", 9293, 4312, 6478, 483, 0, 0, 0, 0]];
+amigo.data.statistics.annotation_overview = [["Source", "similarity evidence", "experimental evidence", "curator inference", "author statement", "combinatorial evidence", "genomic context evidence", "biological system reconstruction", "imported information"], ["MGI", 52439, 53862, 33194, 0, 0, 0, 0, 0], ["ZFIN", 0, 10201, 11115, 0, 0, 0, 0, 0], ["PomBase", 10121, 14791, 3570, 2665, 0, 0, 0, 0], ["dictyBase", 9293, 4312, 6478, 483, 0, 0, 0, 0]];
