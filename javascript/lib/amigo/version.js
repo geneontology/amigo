9701e2f71b19ceece0c922be12505e50bf38a663
SC <github.com@genkisugi.net>
2013-05-09 15:02:49 -0700

    simplified grid landing layout; Fixes #16

--- javascript/lib/amigo/version.js
+++ javascript/lib/amigo/version.js
@@ -24,4 +24,4 @@ amigo.version.revision = "0.9";
  *
  * Partial version for this library: release (date-like) information.
  */
-amigo.version.release = "20130508";
+amigo.version.release = "20130509";
