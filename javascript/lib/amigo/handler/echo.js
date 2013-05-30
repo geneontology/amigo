a5e7ce84bec69a92dc0b6838ed9c9f273a659730
SC <github.com@genkisugi.net>
2013-05-29 18:27:26 -0700

    new handler format; amigo-specific mechanism running out of json structure (with memoization of string to function evals); addition of demo echo handler; tests

---
+++ javascript/lib/amigo/handler/echo.js
@@ -0,0 +1,49 @@
+/* 
+ * Package: echo.js
+ * 
+ * Namespace: amigo.handler.echo
+ * 
+ * Static function handler for echoing inputs--really used for
+ * teaching and testing.
+ */
+
+// Setup the internal requirements.
+bbop.core.require('bbop', 'core');
+//bbop.core.require('amigo', 'linker');
+bbop.core.namespace('amigo', 'handler', 'echo');
+
+/*
+ * Function: echo
+ * 
+ * Applies bbop.core.dump to whatever comes in.
+ * 
+ * Parameters:
+ *  thing
+ * 
+ * Returns:
+ *  a string; it /will/ be a string
+ * 
+ * Also See: <bbop.handler>
+ */
+amigo.handler.echo = function(thing, name, context){
+
+    // Force a return string into existence.
+    var retstr = null;
+    try {
+	retstr = bbop.core.dump(thing);
+    } catch (x) {
+	retstr = '';
+    }
+
+    // // Appaend any optional stuff.
+    // var is_def = bbop.core.is_defined;
+    // var what = bbop.core.what_is;
+    // if( is_def(name) && what(name) == 'string' ){
+    // 	retstr += ' (' + name + ')';
+    // }
+    // if( is_def(context) && what(context) == 'string' ){
+    // 	retstr += ' (' + context + ')';
+    // }
+
+    return retstr;
+};
