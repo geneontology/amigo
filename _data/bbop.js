0af8aac0e60a8fbebfcd7c6bbf9aba0c2611f17f
SC <github.com@genkisugi.net>
2013-07-10 14:28:50 -0700

    bump

--- _data/bbop.js
+++ _data/bbop.js
@@ -1361,7 +1361,7 @@ bbop.version.revision = "2.0b1";
  *
  * Partial version for this library: release (date-like) information.
  */
-bbop.version.release = "20130709";
+bbop.version.release = "20130710";
 /* 
  * Package: json.js
  * 
--- javascript/lib/amigo/version.js
+++ javascript/lib/amigo/version.js
@@ -24,4 +24,4 @@ amigo.version.revision = "2.0b1";
  *
  * Partial version for this library: release (date-like) information.
  */
-amigo.version.release = "20130709";
+amigo.version.release = "20130710";
