7d974299ebfe94107d66b7f5b57cf8021f03aab7
SC <github.com@genkisugi.net>
2013-09-06 17:55:57 -0700

    bump

--- _data/bbop.js
+++ _data/bbop.js
@@ -1361,7 +1361,7 @@ bbop.version.revision = "2.0b1";
  *
  * Partial version for this library: release (date-like) information.
  */
-bbop.version.release = "20130905";
+bbop.version.release = "20130906";
 /* 
  * Package: json.js
  * 
