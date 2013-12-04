2f5a0fa2d925c788d98cfa01575ad15017e6f534
SC <github.com@genkisugi.net>
2013-12-04 13:32:39 -0800

    bump

--- javascript/npm/amigo2/amigo2.js
+++ javascript/npm/amigo2/amigo2.js
@@ -31,7 +31,7 @@ amigo.version.revision = "2.0.0-rc1";
  *
  * Partial version for this library: release (date-like) information.
  */
-amigo.version.release = "20131202";
+amigo.version.release = "20131204";
 /*
  * Package: api.js
  * 
