073dd19aa1c427ef4e7be19a3572d21d9292680f
SC <github.com@genkisugi.net>
2013-12-13 19:21:52 -0800

    bump

--- javascript/lib/amigo/version.js
+++ javascript/lib/amigo/version.js
@@ -24,4 +24,4 @@ amigo.version.revision = "2.0.0-rc1";
  *
  * Partial version for this library: release (date-like) information.
  */
-amigo.version.release = "20131210";
+amigo.version.release = "20131213";
