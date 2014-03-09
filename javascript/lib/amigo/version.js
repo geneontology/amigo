3f4e90b8f40a2fab324c4e717137c7d61abe0ff4
SC <github.com@genkisugi.net>
2014-03-09 03:45:32 -0700

    bump

--- javascript/lib/amigo/version.js
+++ javascript/lib/amigo/version.js
@@ -24,4 +24,4 @@ amigo.version.revision = "2.0.2";
  *
  * Partial version for this library: release (date-like) information.
  */
-amigo.version.release = "20140306";
+amigo.version.release = "20140309";
