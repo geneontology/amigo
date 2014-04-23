1419668f4449630a95ec1e98b5a9eb6b58c5b026
SC <github.com@genkisugi.net>
2014-04-23 14:54:24 -0700

    bump

--- _data/bbop.js
+++ _data/bbop.js
@@ -1884,7 +1884,7 @@ bbop.version.revision = "2.0.3";
  *
  * Partial version for this library: release (date-like) information.
  */
-bbop.version.release = "20140422";
+bbop.version.release = "20140423";
 /*
  * Package: logger.js
  * 
