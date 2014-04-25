efda37adb3fc5b304a2ad8449cbb09a56aa4bf2a
SC <github.com@genkisugi.net>
2014-04-24 21:41:23 -0700

    language

--- javascript/web/LandingGraphs.js
+++ javascript/web/LandingGraphs.js
@@ -1,9 +1,5 @@
 ////
-//// A twiddle to see if I can get Google Charts and BBOP playing
-//// nicely. They seem to want to fight over the initialization.
-////
-//// Trying to let Google win--when it doesn't it seems to throw a fit
-//// and redirect to nothingness. WTF, Google?
+//// ...
 ////
 
 function LandingGraphsInit(){
