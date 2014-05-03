c31d220e359f6de79baecf22db6f716c43a6340e
SC <github.com@genkisugi.net>
2014-05-02 22:17:36 -0700

    doc bump

--- perl/docs/javascript/searchdata.js
+++ perl/docs/javascript/searchdata.js
@@ -93,7 +93,7 @@ var indexSectionsWithContent = {
       "Symbols": false,
       "Numbers": false,
       "A": true,
-      "B": false,
+      "B": true,
       "C": true,
       "D": true,
       "E": true,
