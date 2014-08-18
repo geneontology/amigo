d4848092650294a0ee4191a2abb3995b087314e4
SC <github.com@genkisugi.net>
2014-08-17 22:14:18 -0700

    apparently I had a fundamental misunderstanding of semver--no latest; bumping forward to 2.2.2, with lock on new bbop-js

--- javascript/lib/amigo/version.js
+++ javascript/lib/amigo/version.js
@@ -24,4 +24,4 @@ amigo.version.revision = "2.2.2";
  *
  * Partial version for this library: release (date-like) information.
  */
-amigo.version.release = "20140811";
+amigo.version.release = "20140817";
