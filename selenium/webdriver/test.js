4257b24933ae2f14e76f19d27eebfbc68f17658f
SC <github.com@genkisugi.net>
2015-02-20 00:10:29 -0800

    some more testing doc; fixed some of the amigo tests; started on headless webdriver test experiment

---
+++ selenium/webdriver/test.js
@@ -0,0 +1,13 @@
+
+
+var By = require('selenium-webdriver').By,
+until = require('selenium-webdriver').until,
+firefox = require('selenium-webdriver/firefox');
+
+var driver = new firefox.Driver();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
