982533ca5e05a437f70dba03add066ad690c04b7
SC <github.com@genkisugi.net>
2015-02-23 21:10:35 -0800

    exp now minimally passing; #176

--- selenium/webdriver/exp_tests.js
+++ selenium/webdriver/exp_tests.js
@@ -2,11 +2,11 @@
 //// Experimental test playground.
 ////
 
-// Browser setup and start.
 var By = require('selenium-webdriver').By;
 var until = require('selenium-webdriver').until;
 var firefox = require('selenium-webdriver/firefox');
-var driver = new firefox.Driver();
+var test = require('selenium-webdriver/testing');
+var assert = require('chai').assert;
 
 // Get which AmiGO we want from the env, or default.
 var target = 'http://amigo.geneontology.org/';
@@ -14,32 +14,56 @@ if( process.env['AMIGO'] ){
     target = process.env['AMIGO'];
 }
 
-// // Example of just going to a page and checking something.
-// // Front page.
-// driver.get(target);
-// driver.wait(until.titleIs('AmiGO 2: Welcome'), 1000);
+test.describe('AmiGO 2 (experimental tests)', function(){
+    
+    // Pre-run.
+    var driver;
+    test.before(function() {
+	driver = new firefox.Driver();
+    });
+    
+    test.it('pass', function(){
+	assert.equal(1, 1, 'passed');
+    });
 
-// // Example of a form submission and simple check.
-// // Medial search from autocomplete.
+    // TODO!
+
+    // Post-run.
+    test.after(function(){
+	driver.quit();
+    });
+});
+
+test.describe('AmiGO 2 (autocomplete/search)', function(){
+    
+    // Pre-run.
+    var driver;
+    test.before(function() {
+	driver = new firefox.Driver();
+    });
+    
+    // Example of a form submission and simple check.
+    // Medial search from autocomplete.
+    test.it('functioning bioentiry data get', function(){
+	driver.get(target);
+	driver.findElement(By.id('gsf-query')).sendKeys('neurogenesis');
+	driver.findElement(By.id('query-form')).submit();
+	driver.wait(until.titleIs('AmiGO 2: Search Directory'), 1000);
+    });
+
+// // Example of checking for the existance of something in a dynamic
+// // dropdown.
+// // Autocomplete for "neurogenesis" (check that dropdown occurs with
+// // GO:0022008).
 // driver.get(target);
 // driver.findElement(By.id('gsf-query')).sendKeys('neurogenesis');
-// driver.findElement(By.id('query-form')).submit();
-// driver.wait(until.titleIs('AmiGO 2: Search Directory'), 1000);
+// //var ul = driver.findElement(By.className('ui-autocomplete'));
+// //ul.wait(until.elementTextContains('GO:0022008'), 1000);
+// console.log(  driver.wait(until.elementLocated(By.className('ui-autocomplete')), 1000).then(function(){ console.log('foo'); })  );
+// //driver.getText
 
-///
-/// Work.
-///
-
-// Example of checking for the existance of something in a dynamic
-// dropdown.
-// Autocomplete for "neurogenesis" (check that dropdown occurs with
-// GO:0022008).
-driver.get(target);
-driver.findElement(By.id('gsf-query')).sendKeys('neurogenesis');
-//var ul = driver.findElement(By.className('ui-autocomplete'));
-//ul.wait(until.elementTextContains('GO:0022008'), 1000);
-console.log(  driver.wait(until.elementLocated(By.className('ui-autocomplete')), 1000).then(function(){ console.log('foo'); })  );
-driver.getText
+// assert.equals(1, 1);
+// });
 
 //.then(function(foo){ console.log('boom: ', foo); foo.findElement(By.className('ui-autocomplete')).getText(); });
 // 	foo.getText().then(
@@ -59,5 +83,8 @@ driver.getText
     // return text.search('GO:xxx') !== -1;
 //});
 
-// Done.
-driver.quit();
+    // Post-run.
+    test.after(function(){
+	driver.quit();
+    });
+});
