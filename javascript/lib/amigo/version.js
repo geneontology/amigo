8d8222e17e9aa29bb56bf6cce9d39a285aa7cde3
SC <github.com@genkisugi.net>
2015-03-16 13:40:41 -0700

    bump

---
+++ javascript/lib/amigo/version.js
@@ -0,0 +1,27 @@
+/* 
+ * Package: version.js
+ * 
+ * Namespace: amigo.version
+ * 
+ * This package was automatically generated during the build process
+ * and contains its version information--this is the release of the
+ * API that you have.
+ */
+
+if ( typeof amigo == "undefined" ){ var amigo = {}; }
+if ( typeof amigo.version == "undefined" ){ amigo.version = {}; }
+
+/*
+ * Variable: revision
+ *
+ * Partial version for this library; revision (major/minor version numbers)
+ * information.
+ */
+amigo.version.revision = "2.2.0";
+
+/*
+ * Variable: release
+ *
+ * Partial version for this library: release (date-like) information.
+ */
+amigo.version.release = "20150316";
