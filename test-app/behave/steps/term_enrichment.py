f43bff46057c7a4fbba95e9675676bf874792516
SC <github.com@genkisugi.net>
2015-10-19 15:07:14 -0700

    fix for xpath for rte submit button; I will never understand why this always shifts

--- test-app/behave/steps/term_enrichment.py
+++ test-app/behave/steps/term_enrichment.py
@@ -23,7 +23,7 @@ def step_impl(context):
     ## Current 2.4.x location.
     xp_24x = "/html/body/div[2]/div[4]/div/div/form/div[2]/button"
     ## Current 2.3.x location.   
-    xp_23x = "/html/body/div[2]/div[5]/div/div/form/div[2]/button"
+    xp_23x = "/html/body/div[2]/div[3]/div/div/form/div[2]/button"
     
 
     ## Try them both, starting with 2.4.x
