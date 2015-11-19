ca425ad8202babb9cdae4da206f3ad99a4a66608
SC <github.com@genkisugi.net>
2015-11-19 01:01:46 -0800

    a little more work on, and possibly a first pass at, #282

--- scripts/gass.js
+++ scripts/gass.js
@@ -22,6 +22,10 @@ var path = require('path');
 var us = require('underscore');
 var yaml = require('yamljs');
 
+// Templating.
+var md = require('markdown');
+
+
 ///
 /// Helpers and aliases.
 ///
@@ -72,25 +76,33 @@ var app = express();
 app.use(bodyParser.urlencoded({'extended': true}));
 
 // Homepage!
-app.get('/', function (req, res) {
-    res.send('Are you feeling GASSy?');
+app.get('/', function (req, res){
+
+    // Grab markdown renderable file.
+    var landing_raw = fs.readFileSync('./scripts/gass.md').toString();
+    var landing_md = md.markdown.toHTML(landing_raw);
+    res.send(landing_md);
 });
 
-// Initial service.
-app.get('/gene-to-term', function (req, res) {
+// 
+app.get('/gene-to-term', function (req, res){
 
-    var ret = {status: 'fail'};
+    var ret = {
+	service: 'gene-to-term',
+	status: 'fail'
+    };
 
     // req.stringify(req.query); GET
     // JSON.stringify(req.body) POST
     // Get our query terms.
     if( req.query && req.query['q'] && req.query['q'].length !== 0 ){
 	
-	// Input as list.
+	// Input as list, remove dupes.
 	var gp_accs = req.query['q'];
 	if( ! us.isArray(gp_accs) ){
 	    gp_accs = [gp_accs];
 	}
+	gp_accs = us.uniq(gp_accs);
 
 	// Next, setup the manager environment.
 	ll('Setting up manager.');
@@ -121,6 +133,7 @@ app.get('/gene-to-term', function (req, res) {
 	
 	// Fetch the data and grab the number we want.
 	var gp_info = {};
+	var term_info = {};
 	var accumulator_fun = function(resp){	
 	    
 	    // Who was this?
@@ -134,26 +147,35 @@ app.get('/gene-to-term', function (req, res) {
 		if( fq.substr(0, 9) === 'bioentity' ){
 		    acc = fq.substr(10, fq.length-1);
 		    acc = bbop.dequote(acc);
-		    ll('Looking at info for: ' + acc);
+		    //ll('Looking at info for: ' + acc);
 		}
 	    });
 	    
 	    if( acc ){
 		//ll('Looking at info for: ' + acc);
-		console.log(resp);
+		//console.log(resp);
 		
 		var ffs = resp.facet_field('regulates_closure');
 		each(ffs, function(pair){
 		    
-		    console.log(pair);
+		    //console.log(pair);
 		    
+		    var tid = pair[0];
+		    var acnt = pair[1];
+
 		    // Ensure existance.
 		    if( ! gp_info[acc] ){ 
 			gp_info[acc] = {};
 		    }
+		    if( ! term_info[tid] ){ 
+			term_info[tid] = 0;
+		    }
 		    
-		    //
-		    gp_info[acc][pair[0]] = pair[1];
+		    // Add gp info
+		    gp_info[acc][tid] = acnt;
+		    
+		    // Add term info
+		    term_info[tid] = term_info[tid] +1;
 		});
 	    }
 	    
@@ -165,10 +187,11 @@ app.get('/gene-to-term', function (req, res) {
 	    
 	    console.log('gp_info: ', gp_info);
 	    
-	    ret = {
-		'status': 'success',
-		'q': gp_accs,
-		'summary': gp_info
+	    ret['status'] = 'success';
+	    ret['q'] = gp_accs;
+	    ret['summary'] = {
+		'gene-to-term-summary-count': term_info//,
+		//'gene-to-term-annotation-count': gp_info
 	    };
 	    res.json(ret);
 
@@ -194,6 +217,164 @@ app.get('/gene-to-term', function (req, res) {
 	final_fun();
     }
 });
+
+// 
+app.get('/term-to-gene', function (req, res){
+
+    var ret = {
+	service: 'term-to-gene',
+	status: 'fail'
+    };
+
+    // req.stringify(req.query); GET
+    // JSON.stringify(req.body) POST
+    // Get our query terms.
+    if( req.query && req.query['q'] && req.query['q'].length !== 0 ){
+	
+	// Input as list, remove dupes.
+	var term_accs = req.query['q'];
+	if( ! us.isArray(term_accs) ){
+	    term_accs = [term_accs];
+	}
+	term_accs = us.uniq(term_accs);
+
+	// Next, setup the manager environment.
+	ll('Setting up manager.');
+	var gconf = new golr_conf.conf(amigo.data.golr);
+	var engine = new sync_engine(golr_response);
+	var go = new golr_manager(golr_url, gconf, engine, 'sync');
+	go.set_personality('bioentity'); // always this
+	//go.debug(false);
+
+	// Now, cycle though all of the gps to collect info on.
+	ll('Gathering batch URLs for bioentity/term data...');
+	each(term_accs, function(acc, index){
+	    
+    	    // Set/reset for the next query.
+    	    go.reset_query_filters(); // reset from the last iteration
+	    
+	    // 
+	    go.add_query_filter('document_category', 'bioentity', ['*']);
+	    go.add_query_filter('regulates_closure', acc);
+	    //go.add_query_filter('taxon', taxon_filter, ['*']); }
+	    go.set('rows', 0); // care not about rows
+	    go.set_facet_limit(0); // care not about facets
+
+	    // Remember for the batching.
+    	    go.add_to_batch();
+	});
+	
+	// Fetch the data and grab the number we want.
+	var term_info = {};
+	var accumulator_fun = function(resp){	
+	    
+	    // Who was this?
+	    var acc = null;
+	    var fqs = resp.parameter('fq');
+	    //console.log(fqs);
+	    //console.log(resp);
+	    each(fqs, function(fq){
+		//console.log(fq);
+		//console.log(fq.substr(0, 17));
+		if( fq.substr(0, 17) === 'regulates_closure' ){
+		    acc = fq.substr(18, fq.length-1);
+		    acc = bbop.dequote(acc);
+		    ll('Looking at info for: ' + acc);
+		}
+	    });
+	    
+	    if( acc ){
+		var total = resp.total_documents();
+		term_info[acc] = total;
+	    }
+	    
+	};
+
+	// The final function is the data renderer.
+	var final_fun = function(){
+	    ll('Starting final in stage 01...');
+	    
+	    console.log('term_info: ', term_info);
+	    
+	    ret['status'] = 'success';
+	    ret['q'] = term_accs;
+	    ret['summary'] = {
+		'term-to-gene-summary-count': term_info
+	    };
+	    res.json(ret);
+
+	    ll('Completed stage 01!');
+	};
+
+	ll('Start "batch" run.');
+	//go.run_batch(accumulator_fun, final_fun);
+	var done_p = false;
+	while( ! done_p ){
+	    var next_url = go.next_batch_url();
+	    if( ! next_url ){
+		done_p = true;
+	    }else{
+
+		// BUG/TODO: Hack.
+		console.log(next_url);
+		var resp = go._runner(next_url + '&callback_type=search');
+		accumulator_fun(resp);
+	    }
+	}
+
+	final_fun();
+    }
+});
+
+app.get('/overview', function (req, res){
+
+    var ret = {
+	service: 'overview',
+	status: 'fail'
+    };
+
+    // Setup the manager environment.
+    ll('Setting up manager.');
+    var gconf = new golr_conf.conf(amigo.data.golr);
+    var engine = new sync_engine(golr_response);
+    var go = new golr_manager(golr_url, gconf, engine, 'sync');
+    go.set_personality('bioentity'); // always this
+    //go.debug(false);
+
+    // Set/reset for the next query.
+    go.reset_query_filters(); // reset from the last iteration	    
+    go.add_query_filter('document_category', 'bioentity');
+    go.set('rows', 0); // care not about rows
+    go.set_facet_limit(0); // care not about facets
+    var b_resp = go.search();
+    var total_gps = b_resp.total_documents();
+
+    // Set/reset for the next query.
+    go.reset_query_filters(); // reset from the last iteration	    
+    go.add_query_filter('document_category', 'ontology_class');
+    go.set('rows', 0); // care not about rows
+    go.set_facet_limit(0); // care not about facets
+    var t_resp = go.search();
+    var total_terms = t_resp.total_documents();
+	
+    // Set/reset for the next query.
+    go.reset_query_filters(); // reset from the last iteration	    
+    go.add_query_filter('document_category', 'annotation');
+    go.set('rows', 0); // care not about rows
+    go.set_facet_limit(0); // care not about facets
+    var a_resp = go.search();
+    var total_anns = a_resp.total_documents();
+	
+    // Reponse
+    ret['status'] = 'success';
+    ret['summary'] = {
+	'term-count': total_terms,
+	'gene-product-count': total_gps,
+	'annotation-count': total_anns
+    };
+    res.json(ret);    
+});
+
 
 // Spin up.
 app.listen(port);
