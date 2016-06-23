3a195d72148cd3d0b6210a5bb3aebf5edd4efe75
SC <github.com@genkisugi.net>
2016-06-23 14:25:22 -0700

    giving up on the firefox behave webdriver/runner for right now, will switch back when stablized; making phantomjs default for the time being

--- test-app/behave/environment.py
+++ test-app/behave/environment.py
@@ -6,6 +6,12 @@
 import os
 from selenium import webdriver
 
+# ## From: https://developer.mozilla.org/en-US/docs/Mozilla/QA/Marionette/WebDriver
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# ffcaps = DesiredCapabilities.FIREFOX
+# ffcaps["marionette"] = True
+# ffcaps["binary"] = "/home/sjcarbon/local/src/tarballs/firefox/firefox"
+
 ## Run this before anything else.
 def before_all(context):
     ## Determine the server target. Default: http://amigo.geneontology.org.
@@ -13,11 +19,14 @@ def before_all(context):
     if 'TARGET' in os.environ:
         context.target = os.environ['TARGET']
     ## Get the browser we're going to use. Default: firefox.
-    if 'BROWSER' in os.environ and os.environ['BROWSER'] == 'phantomjs':
-        context.browser = webdriver.PhantomJS()
+    if 'BROWSER' in os.environ and os.environ['BROWSER'] == 'firefox':
+        context.browser = webdriver.Firefox(capabilities=ffcaps)
     else:
-        context.browser = webdriver.Firefox()
+        context.browser = webdriver.PhantomJS()
 
+    ## Set a decent timeout: 30s.
+    context.browser.set_page_load_timeout(30)
+        
 ## Do this after completing everything.
 def after_all(context):
     context.browser.quit()
