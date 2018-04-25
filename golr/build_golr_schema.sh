a51e7f31b27fbe1b854f7cfb11caf555b17738a5
Nathan Dunn <ndunn@me.com>
2018-04-25 11:26:30 -0700

    removed errant file

--- golr/build_golr_schema.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-~/git/OWLTools-GO-svn/bin/owltools -solr-config ~/git/amigo/metadata/ont-config.yaml ~/git/amigo/metadata/bio-config.yaml ~/git/amigo/metadata/ann-config.yaml ~/git/amigo/metadata/ann_ev_agg-config.yaml -solr-schema-dump > ~/git/amigo/golr/solr/conf/2-schema.xml
