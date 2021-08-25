b3494e4a3416db375967537b56d9814b084c03ad
abessiari <aessiari@lbl.gov>
2021-08-25 13:09:56 -0700

    added aws terraform scripts and apache proxy confs

---
+++ provision/aws/security_groups.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "amigo_sg" {
+  vpc_id = aws_vpc.amigo_app_stack_vpc.id
+  tags   = var.tags
+
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9999 
+    to_port     = 9999 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
